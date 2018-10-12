8ddcd9ede118180d52ce54511f9b3fa5fefba420
nmaslov <nmaslov255@yandex.ru>
2018-10-12 09:28:34 +0300

    http request module has been made

---
+++ parser/api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import requests
+
+URL = 'https://президентскиегранты.рф/public/application/table'
+# URL = 'https://xn--80afcdbalict6afooklqi5o.xn--p1ai/public/application/table'
+
+# I don't know why, but these empty params are required
+DEFAULT_PARAMS = {
+    'SearchString':'',
+    'DirectionId':'',
+}
+
+# I'm Browser. Without it i can't get content :(
+HEADERS = {
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_0) \
+                  AppleWebKit/537.36 (KHTML, like Gecko) \
+                  Chrome/69.0.3497.100 Safari/537.36'
+}
+
+def request(URL, params):
+    params = {**DEFAULT_PARAMS, **params}
+
+    response = requests.get(URL, params=params, headers=HEADERS)
+    
+    if response.status_code != 200:
+        response.raise_for_status()
+    return response
--- parser/main.py
+++ parser/main.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+import api
+
+if __name__ == '__main__':
+    table_settings = {
+        'OnlyWinners': 'true',
+        'CompetitionId': 3,
+    }
+
+    response = api.request(api.URL, table_settings)
+    html = response.text
+    print(html)
