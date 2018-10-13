c0eff09cfe15aa2de0435fe0f7de09ebf34fd7c2
nmaslov <nmaslov255@yandex.ru>
2018-10-13 20:11:04 +0300

    total count of pages

--- parser/main.py
+++ parser/main.py
@@ -6,3 +6,4 @@ import parser
 
 if __name__ == '__main__':
     response = api.request(api.URL, cli.SETTING)
+    print(parser.get_total_count_of_pages(response.text))
--- parser/parser.py
+++ parser/parser.py
@@ -33,3 +33,19 @@ def is_empty_page(text):
     if html.find('h2', class_='application-not-found') is None:
         return False
     return True
+
+def get_total_count_of_pages(text):
+    """
+    Arguments:
+        text {str} -- Raw html string
+    
+    Returns:
+        int -- total count of pages
+    """
+    html = BeautifulSoup(text, 'html.parser')
+
+    count_pages = html.find('ul', class_='pagination')\
+                      .find_all('li')[-1].a.string
+
+    return int(count_pages)
+    
